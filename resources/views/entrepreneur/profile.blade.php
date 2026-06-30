b75e7dc00391bc0fc6582ea971772fd076525afd
Muhammad0UmarFarooq <umar.eufo@gmail.com>
2026-06-30 16:14:13 +0500

    profile little changes

--- resources/views/entrepreneur/profile.blade.php
+++ resources/views/entrepreneur/profile.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Profile - Naeem Azhar</title>
+    <title>Profile - {{ auth()->user()->name }}</title>
     
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
