c7ca2d342b9324c70cd41766262b616f41b8f7d2
Muhammad0UmarFarooq <umar.eufo@gmail.com>
2026-08-31 12:58:26 +0500

    add fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#0b1120">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <title>{{ config('app.name', 'InvestBridge') }}</title>
 
     <!-- Fonts -->
